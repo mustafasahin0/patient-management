07a2935fd517f19ceffd8914ffa9fa17db53ff3c
mustafasahin0 <sahinmustafaenes4@gmail.com>
2025-05-18 11:05:24 -0600

    Implement patient deletion and update email existence check

--- patient-service/src/main/java/com/aavukat/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/aavukat/patientservice/controller/PatientController.java
@@ -39,5 +39,11 @@ public class PatientController {
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
         return ResponseEntity.ok(patientResponseDTO);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
+    }
 }
 
